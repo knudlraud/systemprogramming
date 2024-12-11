f5fa01f772b4a7063c22ef302cab2c555666238d
Dorcopsis <42609918+Dorcopsis@users.noreply.github.com>
2024-12-11 16:29:17 +0900

    Update moni.c

--- moni.c
+++ moni.c
@@ -6,25 +6,9 @@
  * 2024 fall semester sysytem programming project.
  * Backing up and monitoring with visualize part of project.
  *************************************************************/
+#include "moni.h"
 
 
-
-#include <locale.h>
-#include <ncurses.h>
-#include <string.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <sys/inotify.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <limits.h>
-#include <errno.h>
-#include <time.h>
-
-#define EVENT_SIZE  (sizeof(struct inotify_event))
-#define BUF_LEN     (1024 * (EVENT_SIZE + 16))
-#define MAX_DIRS    10 // Maximum number of directories to monitor
 
 void rotate_log_file(const char* log_filepath) {
     struct stat st;
